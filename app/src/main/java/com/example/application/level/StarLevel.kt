a0258afd6228634e6e100d0616a07b6f2f9b4051
Gloff228 <romo4ka101@gmail.com>
2023-04-13 22:37:27 +0300

    test2

--- app/src/main/java/com/example/application/level/StarLevel.kt
+++ app/src/main/java/com/example/application/level/StarLevel.kt
@@ -5,13 +5,14 @@ import android.view.View
 import android.view.animation.Animation
 import android.view.animation.ScaleAnimation
 import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.application.MyActivity
 import com.example.application.R
-import kotlin.random.Random
 
 class StarLevel : MyActivity() {
 
-    private lateinit var stars: MutableList<ImageView>
+    //private lateinit var stars: MutableList<ImageView>
     private var currentStar = 1
     private val maxCount = 10
 
@@ -24,34 +25,38 @@ class StarLevel : MyActivity() {
 //        val star3 = findViewById<ImageView>(R.id.star3)
 //        val star4 = findViewById<ImageView>(R.id.star4)
 //        val star5 = findViewById<ImageView>(R.id.star5)
+        // Создаем список звезд
+        //stars = mutableListOf(star1, star2, star3, star4, star5)
 
-        val layout = findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.layout) // здесь R.id.layout - идентификатор вашего LinearLayout из XML разметки
-        val random = Random(1)
 
-        val sstars = arrayOfNulls<ImageView>(maxCount)
+        val layout = findViewById<ConstraintLayout>(R.id.layout)
+        val random = java.util.Random()
 
-        for (i in 1..maxCount) { // здесь 10 - это количество звезд, которые вы хотите добавить
+        val stars = arrayOfNulls<ImageView>(maxCount)
+
+        for (i in 1..maxCount) {
             val star = ImageView(this)
-            star.layoutParams.width = 20
-            star.layoutParams.height = 20
-            star.setImageResource(R.drawable.star) // устанавливаем изображение звезды
-            star.isClickable = true // делаем звезду кликабельной
+            val layoutParams = LinearLayout.LayoutParams(
+                100, // Ширина
+                100 // Высота
+            )
+            star.layoutParams = layoutParams
+            star.setImageResource(R.drawable.star)
+            star.isClickable = true
 
             // устанавливаем случайные координаты звезды
-            val x = random.nextInt(layout.width - star.width)
-            val y = random.nextInt(layout.height - star.height)
+            val x = random.nextInt(1000)
+            val y = random.nextInt(600)
             star.x = x.toFloat()
             star.y = y.toFloat()
 
-            stars[i] = star
+            stars[i - 1] = star
         }
-        for (star in sstars) {
+        for (star in stars) {
             layout.addView(star)
         }
 
 
-        // Создаем список звезд
-        //stars = mutableListOf(star1, star2, star3, star4, star5)
 
         fun createAnimation(star: ImageView) {
             val anim = ScaleAnimation(
@@ -68,6 +73,7 @@ class StarLevel : MyActivity() {
                         star.scaleY = 6f // новый масштаб по Y
                         animation?.cancel()
                     }
+
                     override fun onAnimationEnd(animation: Animation?) {
 
                     }
@@ -78,14 +84,14 @@ class StarLevel : MyActivity() {
             star.setOnClickListener {
                 star.visibility = View.GONE
                 currentStar++
-                if (currentStar <= 5) {
-                    createAnimation(stars[currentStar - 1])
+                if (currentStar <= maxCount) {
+                    createAnimation(stars[currentStar - 1]!!)
                 } else {
                     finish()
                 }
             }
         }
 
-        createAnimation(sstars[0]!!)
+        createAnimation(stars[0]!!)
     }
 }
