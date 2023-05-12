a5daab540365498908f0cf5250417b5965f831fb
Daria <daria.v2906@gmail.com>
2023-05-12 20:23:09 +0300

    Настройки

--- app/src/main/java/com/example/application/level/bubblelevel/BubbleLevel.kt
+++ app/src/main/java/com/example/application/level/bubblelevel/BubbleLevel.kt
@@ -14,29 +14,31 @@ import com.example.application.R
 import com.example.application.level.AbstractLevelActivity
 import com.example.application.level.figures.FigureBubble
 
+var bubSpeed: Int = -1
 class BubbleLevel: AbstractLevelActivity() {
 
     private lateinit var bubbleList: MutableList<FigureBubble>
     lateinit var currentBubble: FigureBubble
-    private val bubbleSize = 4 * coefficientForSize + 350
     var bubbleCounter = 0
+    var figuresNumber: Int = -1
+    var figureSize: Int = -1
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bubblelevel)
 
+        getSettings()
         setCounter()
+        createImage()
+
         surface = findViewById(R.id.bubSurface)
         surface.holder.addCallback(this)
         surface.setZOrderOnTop(true)
         surface.holder.setFormat(PixelFormat.TRANSPARENT)
-
     }
 
-    override fun onResume() {
-        super.onResume()
-
+    private fun createImage() {
         val bubble1 = BitmapFactory.decodeResource(this.resources, R.drawable.bubble_first)
         val bubble2 = BitmapFactory.decodeResource(this.resources, R.drawable.bubble_second)
         val bubble3 = BitmapFactory.decodeResource(this.resources, R.drawable.bubble_third)
@@ -44,11 +46,11 @@ class BubbleLevel: AbstractLevelActivity() {
         val bubble5 = BitmapFactory.decodeResource(this.resources, R.drawable.bubble_fifth)
 
         bubbleList = mutableListOf(
-            FigureBubble(Bitmap.createScaledBitmap(bubble1, bubbleSize, bubbleSize, false)),
-            FigureBubble(Bitmap.createScaledBitmap(bubble2, bubbleSize, bubbleSize, false)),
-            FigureBubble(Bitmap.createScaledBitmap(bubble3, bubbleSize, bubbleSize, false)),
-            FigureBubble(Bitmap.createScaledBitmap(bubble4, bubbleSize, bubbleSize, false)),
-            FigureBubble(Bitmap.createScaledBitmap(bubble5, bubbleSize, bubbleSize, false)),
+            FigureBubble(Bitmap.createScaledBitmap(bubble1, figureSize, figureSize, false)),
+            FigureBubble(Bitmap.createScaledBitmap(bubble2, figureSize, figureSize, false)),
+            FigureBubble(Bitmap.createScaledBitmap(bubble3, figureSize, figureSize, false)),
+            FigureBubble(Bitmap.createScaledBitmap(bubble4, figureSize, figureSize, false)),
+            FigureBubble(Bitmap.createScaledBitmap(bubble5, figureSize, figureSize, false)),
         )
     }
 
@@ -112,24 +114,34 @@ class BubbleLevel: AbstractLevelActivity() {
         bubbleList.removeAt(0)
     }
 
+    private fun getSettings() {
+        figuresNumber = intent.getIntExtra(BubbleSettings.PARAM_FIGURES_NUMBER, 1)
+        figureSize = intent.getIntExtra(BubbleSettings.PARAM_FIGURE_SIZE, 1)
+        bubSpeed = intent.getIntExtra(BubbleSettings.PARAM_SPEED, 1)
+    }
+
     @SuppressLint("SetTextI18n")
     fun setCounter() {
         val textOfCounter = findViewById<TextView>(R.id.counterText)
-        textOfCounter.text = "0/${numberBubbles}"
+        textOfCounter.text = "0/${figuresNumber}"
     }
 
     @SuppressLint("SetTextI18n")
     private fun updateCounter() {
         val textOfCounter = findViewById<TextView>(R.id.counterText)
 
-        if (bubbleCounter < numberBubbles - 1) {
+        if (bubbleCounter < figuresNumber - 1) {
             bubbleCounter++
-            textOfCounter.text = "${bubbleCounter}/${numberBubbles}"
+            textOfCounter.text = "${bubbleCounter}/${figuresNumber}"
         } else finishLevel()
     }
 
     override fun finishLevel() {
-        startActivity(Intent(this, BubbleVictoryScreen::class.java))
+        val intent = Intent(this, BubbleVictoryScreen::class.java)
+        intent.putExtra(BubbleSettings.PARAM_FIGURES_NUMBER, figuresNumber)
+        intent.putExtra(BubbleSettings.PARAM_FIGURE_SIZE, figureSize)
+        intent.putExtra(BubbleSettings.PARAM_SPEED, bubSpeed)
+        startActivity(intent)
         finish()
     }
 
--- app/src/main/java/com/example/application/level/bubblelevel/BubbleVictoryScreen.kt
+++ app/src/main/java/com/example/application/level/bubblelevel/BubbleVictoryScreen.kt
@@ -6,19 +6,31 @@ import android.os.Bundle
 import android.view.View
 import com.example.application.MyActivity
 import com.example.application.R
-import com.example.application.SpecialLevels
 
 class BubbleVictoryScreen: MyActivity() {
 
-
+    var figuresNumber = -1
+    var figureSize = -1
+    var figureSpeed = -1
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bubblelevel_victoryscreen)
+
+        getSettings()
     }
 
+    private fun getSettings() {
+        figuresNumber = intent.getIntExtra(BubbleSettings.PARAM_FIGURES_NUMBER, -1)
+        figureSize = intent.getIntExtra(BubbleSettings.PARAM_FIGURE_SIZE, -1)
+        figureSpeed = intent.getIntExtra(BubbleSettings.PARAM_SPEED, -1)
+    }
     fun onRestartButtonClick(view: View) {
-        startActivity(Intent(this, BubbleLevel::class.java))
+        val intent = Intent(this, BubbleLevel::class.java)
+        intent.putExtra(BubbleSettings.PARAM_FIGURES_NUMBER, figuresNumber)
+        intent.putExtra(BubbleSettings.PARAM_FIGURE_SIZE, figureSize)
+        intent.putExtra(BubbleSettings.PARAM_SPEED, figureSpeed)
+        startActivity(intent)
         finish()
     }
 
