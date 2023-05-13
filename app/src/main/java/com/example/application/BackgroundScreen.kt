00fbf12b6a0d0c48241d71897db6bc7da2bce6fd
FAbrickA <schilonosov.alex@yandex.ru>
2023-05-13 12:46:40 +0300

    Add FigureSettingCircle.kt

--- app/src/main/java/com/example/application/BackgroundScreen.kt
+++ app/src/main/java/com/example/application/BackgroundScreen.kt
@@ -8,13 +8,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import com.example.application.level.figures.FigureComCircle
+import com.example.application.level.figures.FigureSettingCircle
 import com.example.application.utils.BackgroundColor
 
 class BackgroundScreen : MyActivity() {
     class MyView(context: Context) : View(context) {
         override fun onDraw(canvas: Canvas) {
             super.onDraw(canvas)
-            val figure = FigureComCircle(canvas.height * 3)
+            val figure = FigureSettingCircle(canvas.height)
             figure.setPosition(canvas.height/2, canvas.height/2)
             figure.draw(canvas)
         }
