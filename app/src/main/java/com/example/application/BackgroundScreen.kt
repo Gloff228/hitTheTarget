ece9787c3d5fae6faed96477971496c0187fee1d
Gloff228 <romo4ka101@gmail.com>
2023-05-13 10:54:01 +0300

    Превью кружка

--- app/src/main/java/com/example/application/BackgroundScreen.kt
+++ app/src/main/java/com/example/application/BackgroundScreen.kt
@@ -1,15 +1,34 @@
 package com.example.application
 
+import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import com.example.application.level.figures.FigureComCircle
 import com.example.application.utils.BackgroundColor
 
 class BackgroundScreen : MyActivity() {
+    class MyView(context: Context) : View(context) {
+        override fun onDraw(canvas: Canvas) {
+            super.onDraw(canvas)
+            val figure = FigureComCircle(canvas.height * 3)
+            figure.setPosition(canvas.height/2, canvas.height/2)
+            figure.draw(canvas)
+        }
+    }
+    private fun redraw() {
+        val myView = MyView(this)
+        val layout = findViewById<LinearLayout>(R.id.circle)
+        layout.removeAllViews()
+        layout.addView(myView)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_background_screen)
+        redraw()
     }
 
     override fun onStop() {
--- app/src/main/java/com/example/application/TargetColorScreen.kt
+++ app/src/main/java/com/example/application/TargetColorScreen.kt
@@ -1,13 +1,33 @@
 package com.example.application
 
+import android.content.Context
+import android.graphics.Canvas
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
+import com.example.application.level.figures.FigureComCircle
 import com.example.application.utils.TargetColor
-
+import com.example.application.utils.globalSettings
 class TargetColorScreen : MyActivity() {
+    class MyView(context: Context) : View(context) {
+        override fun onDraw(canvas: Canvas) {
+            super.onDraw(canvas)
+            val figure = FigureComCircle(canvas.height * 3)
+            figure.setPosition(canvas.height/2, canvas.height/2)
+            figure.draw(canvas)
+        }
+    }
+    private fun redraw() {
+        val myView = MyView(this)
+        val layout = findViewById<LinearLayout>(R.id.circle)
+        layout.removeAllViews()
+        layout.addView(myView)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_target_color_screen)
+
+        redraw()
     }
 
     override fun onStop() {
@@ -18,38 +38,47 @@ class TargetColorScreen : MyActivity() {
 
     fun selectWhite(view: View) {
         setTargetColor(TargetColor.WHITE)
+        redraw()
     }
 
     fun selectBlack(view: View) {
         setTargetColor(TargetColor.BLACK)
+        redraw()
     }
 
     fun selectPink(view: View) {
         setTargetColor(TargetColor.PINK)
+        redraw()
     }
 
     fun selectRed(view: View) {
         setTargetColor(TargetColor.RED)
+        redraw()
     }
 
     fun selectBlue(view: View) {
         setTargetColor(TargetColor.BLUE)
+        redraw()
     }
 
     fun selectPurple(view: View) {
         setTargetColor(TargetColor.PURPLE)
+        redraw()
     }
 
     fun selectYellow(view: View) {
         setTargetColor(TargetColor.YELLOW)
+        redraw()
     }
 
     fun selectGreen(view: View) {
         setTargetColor(TargetColor.GREEN)
+        redraw()
     }
 
     fun selectDarkGreen(view: View) {
         setTargetColor(TargetColor.DARKGREEN)
+        redraw()
     }
 
     fun onClickButtonSettingsBack(view: View) {
